621af25dca8d1803f127bc2d801ec33c04adc66f
jlandin515 <jimjoe515@gmail.com>
2022-06-30 19:30:01 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,27 +5,19 @@ import pandas as pd
 import streamlit as st
 
 """
-# Welcome to Streamlit!
+# Aplicación de Clasificación de Imágenes de Lenguaje de Señas 
 
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
-
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
-
-In the meantime, below is an example of what you can do with just a few lines of code:
 """
 
 def main():
-	st.title("File Upload Tutorial")
+  st.title("File Upload Tutorial")
 
-	menu = ["Image","Dataset","DocumentFiles","About"]
-	choice = st.sidebar.selectbox("Menu",menu)
+  menu = ["Image","Dataset","DocumentFiles","About"]
+  choice = st.sidebar.selectbox("Menu",menu)
 
-	if choice == "Image":
-		st.subheader("Image")
-
-    elif choice == "Dataset":
-		st.subheader("Dataset")
-
-    elif choice == "DocumentFiles":
-		st.subheader("DocumentFiles")
+  if (choice == "Image"):
+    st.subheader("Image")
+  elif (choice == "Dataset"):
+    st.subheader("Dataset")
+  elif (choice == "DocumentFiles"):
+    st.subheader("DocumentFiles")
