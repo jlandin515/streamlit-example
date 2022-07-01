04f91283e388eb94dcdd2f8066d31894940bc2b1
jlandin515 <jimjoe515@gmail.com>
2022-06-30 19:32:15 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,16 @@ import altair as alt
 import math
 import pandas as pd
 import streamlit as st
+from PIL import Image
 
+'''
+# Aplicación
+
+'''
+
+def load_image(image_file):
+	img = Image.open(image_file)
+	return img
 
 def main():
   st.title("File Upload Tutorial")
@@ -17,3 +26,5 @@ def main():
     st.subheader("Dataset")
   elif (choice == "DocumentFiles"):
     st.subheader("DocumentFiles")
+
+main()
